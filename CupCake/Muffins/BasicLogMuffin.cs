329281c1694230d0b298d4098861bca3d2b3aeb7
Yonom <sepehr1376@gmail.com>
2014-05-19 18:04:13 +0200

    Add BasicLogMuffin

---
+++ CupCake/Muffins/BasicLogMuffin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CupCake.API.Muffins;
+using CupCake.EE.Messages.Receive;
+using CupCake.Log.Log;
+
+namespace CupCake.Muffins
+{
+    public class BasicLogMuffin : CupCakeMuffin
+    {
+        protected override void Enable()
+        {
+            this.EventsPlatform.Event<SayReceiveMessage>().Bind(OnSay);
+        }
+
+        private void OnSay(object sender, SayReceiveMessage e)
+        {
+            this.Logger.Log(LogPriority.Message, e.Text);
+        }
+    }
+}
