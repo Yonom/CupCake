9c2b80e01d7a7d35bcdad1a50afaa1dfaeaa86f4
Yonom <sepehr1376@gmail.com>
2014-05-19 21:37:22 +0200

    Fix chatter is not public

--- CupCake.API/Chatter.cs
+++ CupCake.API/Chatter.cs
@@ -6,7 +6,7 @@ using CupCake.EE.Blocks;
 
 namespace CupCake.API
 {
-    class Chatter
+    public class Chatter
     {
         public ChatService ChatService { get; set; }
         public string Name { get; set; }
