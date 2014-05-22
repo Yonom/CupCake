2471bda755405f28f14381eb2bc33502057eafb1
Yonom <sepehr1376@gmail.com>
2014-05-22 16:53:33 +0200

    Fix misspelled file name

--- CupCake.Core/Services/CupCakeService.cs
+++ CupCake.Core/Services/CupCakeService.cs
@@ -1,6 +1,14 @@
+using System.ComponentModel.Composition;
+using MuffinFramework.Services;
+
 namespace CupCake.Core.Services
 {
     public abstract class CupCakeService : CupCakeService<object>
+    {
+    }
+
+    [InheritedExport(typeof(IService))]
+    public abstract class CupCakeService<TProtocol> : CupCakeServicePart<TProtocol>, IService
     {
     }
 }
