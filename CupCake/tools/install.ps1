6b74e37fdeb6c1d2577a95338dfa8fd548f45f36
Yonom <sepehr1376@gmail.com>
2014-06-04 16:02:37 +0200

    Enable NuGet package restore, start work on NuGet package

---
+++ CupCake/tools/install.ps1
@@ -0,0 +1,13 @@
+param($installPath, $toolsPath, $package, $project)
+
+$project.Properties.Item("PostBuildEvent").Value = "if `$(ConfigurationName) == Debug ""`$(ProjectDir)CupCake.Debug.exe"" `$(TargetDir)"
+
+$asms = $package.AssemblyReferences | %{$_.Name}
+
+foreach ($reference in $project.Object.References)
+{
+    if ($asms -contains $reference.Name + ".dll")
+    {
+        $reference.CopyLocal = $false;
+    }
+}
