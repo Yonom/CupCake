11332fcbee0cf6270e921c3234e061d51ea4d814
Yonom <sepehr1376@gmail.com>
2014-06-04 15:13:28 +0200

    Code cleanup in Debug.exe

--- CupCake.Debug/Program.cs
+++ CupCake.Debug/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Security.Policy;
 using System.Text;
-using System.Threading;
 
 namespace CupCake.Debug
 {
@@ -18,60 +18,11 @@ namespace CupCake.Debug
 
                 if (command == "deploy")
                 {
-                    string cupCakePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\CupCake";
-                    if (!Directory.Exists(cupCakePath))
-                        Directory.CreateDirectory(cupCakePath);
-
-                    string profilesPath = cupCakePath + "\\Profiles";
-                    if (!Directory.Exists(profilesPath))
-                        Directory.CreateDirectory(profilesPath);
-
-                    string debugProfilePath = profilesPath + "\\Debug";
-                    if (Directory.Exists(debugProfilePath))
-                        Directory.Delete(debugProfilePath, true);
-
-                    Directory.CreateDirectory(debugProfilePath);
-
-                    string sourcePath = String.Join(" ", args);
-
-                    string[] files = Directory.GetFiles(sourcePath);
-
-                    foreach (string s in files)
-                    {
-                        string fileName = Path.GetFileName(s);
-                        string destFile = Path.Combine(debugProfilePath, fileName);
-                        File.Copy(s, destFile, true);
-                    }
+                    Deploy(args.Skip(1));
                 }
                 else if (command == "debug")
                 {
-                    string path;
-                    try
-                    {
-                        using (var client = new TcpClient())
-                        {
-                            client.Connect(IPAddress.Loopback, 4570);
-
-                            using (NetworkStream stream = client.GetStream())
-                            using (var reader = new StreamReader(stream, Encoding.Unicode))
-                            {
-                                // Request Debug
-                                stream.WriteByte(0xE1);
-
-                                // Skip one byte
-                                stream.ReadByte();
-                                // Wait until the connection is closed by cupcake
-                                path = reader.ReadToEnd();
-                            }
-                        }
-                    }
-                    catch (SocketException)
-                    {
-                        Console.WriteLine("Problem communicating with the client, make sure it is running.");
-                        return;
-                    }
-
-                    AppDomain.CurrentDomain.ExecuteAssembly(path + "\\CupCake.Server.exe", new[] { "--envpath", path });
+                    Debug();
                 }
                 else
                 {
@@ -82,6 +33,68 @@ namespace CupCake.Debug
             {
                 Console.WriteLine("Please specify a command to run");
             }
+        }
+
+        private static void Deploy(IEnumerable<string> args)
+        {
+            string cupCakePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\CupCake";
+            if (!Directory.Exists(cupCakePath))
+                Directory.CreateDirectory(cupCakePath);
+
+            string profilesPath = cupCakePath + "\\Profiles";
+            if (!Directory.Exists(profilesPath))
+                Directory.CreateDirectory(profilesPath);
+
+            string debugProfilePath = profilesPath + "\\Debug";
+            if (Directory.Exists(debugProfilePath))
+                Directory.Delete(debugProfilePath, true);
+
+            Directory.CreateDirectory(debugProfilePath);
+
+            string sourcePath = String.Join(" ", args);
+
+            string[] files = Directory.GetFiles(sourcePath);
+
+            foreach (string s in files)
+            {
+                string fileName = Path.GetFileName(s);
+                if (fileName != null)
+                {
+                    string destFile = Path.Combine(debugProfilePath, fileName);
+                    File.Copy(s, destFile, true);
+                }
+            }
+        }
+
+        private static void Debug()
+        {
+            string path;
+            try
+            {
+                using (var client = new TcpClient())
+                {
+                    client.Connect(IPAddress.Loopback, 4570);
+
+                    using (NetworkStream stream = client.GetStream())
+                    using (var reader = new StreamReader(stream, Encoding.Unicode))
+                    {
+                        // Request Debug
+                        stream.WriteByte(0xE1);
+
+                        // Skip one byte
+                        stream.ReadByte();
+                        // Wait until the connection is closed by cupcake
+                        path = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Problem communicating with the client, make sure it is running.");
+                return;
+            }
+
+            AppDomain.CurrentDomain.ExecuteAssembly(path + "\\CupCake.Server.exe", new[] { "--envpath", path });
         }
     }
 }
