824e2634d88e0db1dc066114d492922487871658
Yonom <sepehr1376@gmail.com>
2014-06-05 22:36:59 +0200

    Try fix nuget 2

--- CupCake/tools/install.ps1
+++ CupCake/tools/install.ps1
@@ -1,9 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
 $project.Properties.Item("PostBuildEvent").Value = "if `$(ConfigurationName) == Debug ""`$(ProjectDir)CupCake.Debug.exe"" Deploy `$(TargetDir)"
-$project.ConfigurationManager.Item("Debug", "Any CPU").Properties.Item("StartAction").Value = 1;
-$project.ConfigurationManager.Item("Debug", "Any CPU").Properties.Item("StartProgram").Value = "`$(MSBuildProjectDirectory)\CupCake.Debug.exe";
-$project.ConfigurationManager.Item("Debug", "Any CPU").Properties.Item("StartArguments").Value = "Debug";
 
 $asms = $package.AssemblyReferences | %{$_.Name}
 
@@ -14,3 +11,38 @@ foreach ($reference in $project.Object.References)
         $reference.CopyLocal = $false;
     }
 }
+
+
+[xml] $prjXml = Get-Content $project.FullName
+foreach($PropertyGroup in $prjXml.project.ChildNodes)
+{
+	if($PropertyGroup.StartAction -ne $null)
+	{
+		exit
+	}
+}
+
+$propertyGroupElement = $prjXml.CreateElement("PropertyGroup", $prjXml.Project.GetAttribute("xmlns"));
+
+$startActionElement = $prjXml.CreateElement("StartAction", $prjXml.Project.GetAttribute("xmlns"));
+$propertyGroupElement.AppendChild($startActionElement)
+$propertyGroupElement.StartAction = "Program"
+
+$startProgramElement = $prjXml.CreateElement("StartProgram", $prjXml.Project.GetAttribute("xmlns"));
+$propertyGroupElement.AppendChild($startProgramElement)
+$propertyGroupElement.StartProgram = "`$(MSBuildProjectDirectory)\CupCake.Debug.exe"
+
+$startArgumentsElement = $prjXml.CreateElement("StartArguments", $prjXml.Project.GetAttribute("xmlns"));
+$propertyGroupElement.AppendChild($startArgumentsElement)
+$propertyGroupElement.StartArguments = "Debug"
+
+$prjXml.project.AppendChild($propertyGroupElement);
+$writerSettings = new-object System.Xml.XmlWriterSettings
+$writerSettings.OmitXmlDeclaration = $false
+$writerSettings.NewLineOnAttributes = $false
+$writerSettings.Indent = $true
+$projectFilePath = Resolve-Path -Path $project.FullName
+$writer = [System.Xml.XmlWriter]::Create($projectFilePath, $writerSettings)
+$prjXml.WriteTo($writer)
+$writer.Flush()
+$writer.Close()
--- CupCake/tools/uninstall.ps1
+++ CupCake/tools/uninstall.ps1
@@ -1,6 +1,3 @@
 param($installPath, $toolsPath, $package, $project)
 
 $project.Properties.Item("PostBuildEvent").Value = ""
-$project.ConfigurationManager.Item("Debug", "Any CPU").Properties.Item("StartAction").Value = 0;
-$project.ConfigurationManager.Item("Debug", "Any CPU").Properties.Item("StartProgram").Value = "";
-$project.ConfigurationManager.Item("Debug", "Any CPU").Properties.Item("StartArguments").Value = "";
