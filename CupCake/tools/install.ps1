c0b11d118e9484e7ffb3d5bf3b95419ef632b400
Yonom <sepehr1376@gmail.com>
2014-06-05 23:00:57 +0200

    Try fix nuget 3

--- CupCake/tools/install.ps1
+++ CupCake/tools/install.ps1
@@ -11,38 +11,3 @@ foreach ($reference in $project.Object.References)
         $reference.CopyLocal = $false;
     }
 }
-
-
-[xml] $prjXml = Get-Content $project.FullName
-foreach($PropertyGroup in $prjXml.project.ChildNodes)
-{
-	if($PropertyGroup.StartAction -ne $null)
-	{
-		exit
-	}
-}
-
-$propertyGroupElement = $prjXml.CreateElement("PropertyGroup", $prjXml.Project.GetAttribute("xmlns"));
-
-$startActionElement = $prjXml.CreateElement("StartAction", $prjXml.Project.GetAttribute("xmlns"));
-$propertyGroupElement.AppendChild($startActionElement)
-$propertyGroupElement.StartAction = "Program"
-
-$startProgramElement = $prjXml.CreateElement("StartProgram", $prjXml.Project.GetAttribute("xmlns"));
-$propertyGroupElement.AppendChild($startProgramElement)
-$propertyGroupElement.StartProgram = "`$(MSBuildProjectDirectory)\CupCake.Debug.exe"
-
-$startArgumentsElement = $prjXml.CreateElement("StartArguments", $prjXml.Project.GetAttribute("xmlns"));
-$propertyGroupElement.AppendChild($startArgumentsElement)
-$propertyGroupElement.StartArguments = "Debug"
-
-$prjXml.project.AppendChild($propertyGroupElement);
-$writerSettings = new-object System.Xml.XmlWriterSettings
-$writerSettings.OmitXmlDeclaration = $false
-$writerSettings.NewLineOnAttributes = $false
-$writerSettings.Indent = $true
-$projectFilePath = Resolve-Path -Path $project.FullName
-$writer = [System.Xml.XmlWriter]::Create($projectFilePath, $writerSettings)
-$prjXml.WriteTo($writer)
-$writer.Flush()
-$writer.Close()
