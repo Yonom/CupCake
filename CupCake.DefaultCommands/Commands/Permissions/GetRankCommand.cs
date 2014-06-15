9f03d59d8c04bfb389650cf192adc6a809dbe16e
Yonom <sepehr1376@gmail.com>
2014-06-15 03:20:42 +0200

    Make !GetRank return your own rank

--- CupCake.DefaultCommands/Commands/Permissions/GetRankCommand.cs
+++ CupCake.DefaultCommands/Commands/Permissions/GetRankCommand.cs
@@ -7,18 +7,29 @@ namespace CupCake.DefaultCommands.Commands.Permissions
 {
     public sealed class GetRankCommand : PermissionCommandBase
     {
-        [MinArgs(1)]
         [MinGroup(Group.Moderator)]
         [Label("getrank", "getplayerrank")]
-        [CorrectUsage("player")]
+        [CorrectUsage("[player]")]
         protected override void Run(IInvokeSource source, ParsedCommand message)
         {
-            string user = message.Args[0];
-            this.PlayerService.MatchPlayer(user,
-                player =>
-                    source.Reply("{0}'s rank is {1}", player.ChatName, player.GetGroup()),
-                username =>
-                    source.Reply("{0} is now {1}.", PlayerUtils.GetChatName(username), this.Host.GetPermission(username)));
+            if (message.Count >= 1)
+            {
+                string user = message.Args[0];
+                this.PlayerService.MatchPlayer(user,
+                    player =>
+                        source.Reply("{0}'s rank is {1}", player.ChatName, player.GetGroup()),
+                    username =>
+                        source.Reply("{0} is now {1}.", PlayerUtils.GetChatName(username), this.Host.GetPermission(username)));
+            }
+            var playerSource = source as PlayerInvokeSource;
+            if (playerSource != null)
+            {
+                source.Reply("{0}'s rank is {1}", playerSource.Player.ChatName, playerSource.Player.GetGroup());
+            }
+            else
+            {
+                throw new UnknownPlayerCommandException("No player was specified!");
+            }
         }
     }
 }
