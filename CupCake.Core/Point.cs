5e267593a09e9104317edd54c81dff232fc91c53
Yonom <sepehr1376@gmail.com>
2014-07-31 02:20:05 +0200

    Add Equality operators to Point and Rectangle

--- CupCake.Core/Point.cs
+++ CupCake.Core/Point.cs
@@ -2,6 +2,7 @@
 {
     public class Point
     {
+
         public Point(int x, int y)
         {
             this.X = x;
@@ -14,5 +15,36 @@
 
         public int X { get; set; }
         public int Y { get; set; }
+
+        public static bool operator ==(Point left, Point right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Point left, Point right)
+        {
+            return !Equals(left, right);
+        }
+
+        protected bool Equals(Point other)
+        {
+            return this.X == other.X && this.Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Point)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (this.X * 397) ^ this.Y;
+            }
+        }
     }
 }
--- CupCake.Core/Rectangle.cs
+++ CupCake.Core/Rectangle.cs
@@ -18,5 +18,40 @@
         public int Y { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
+
+        protected bool Equals(Rectangle other)
+        {
+            return this.X == other.X && this.Y == other.Y && this.Width == other.Width && this.Height == other.Height;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = this.X;
+                hashCode = (hashCode * 397) ^ this.Y;
+                hashCode = (hashCode * 397) ^ this.Width;
+                hashCode = (hashCode * 397) ^ this.Height;
+                return hashCode;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Rectangle)obj);
+        }
+
+        public static bool operator ==(Rectangle left, Rectangle right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Rectangle left, Rectangle right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
