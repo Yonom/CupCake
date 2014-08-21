39b59d89f0709c53edc2d63cbe114b8cdb20c76d
Yonom <sepehr1376@gmail.com>
2014-08-21 20:03:53 +0200

    Add String.Format extensions to logger

--- CupCake.Core/Log/Logger.cs
+++ CupCake.Core/Log/Logger.cs
@@ -11,11 +11,20 @@
         public LogPlatform LogPlatform { get; private set; }
         public string Name { get; set; }
 
+        /// <summary>
+        /// Logs the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
         public void Log(string message)
         {
             this.Log(LogPriority.Message, message);
         }
 
+        /// <summary>
+        /// Logs the specified message.
+        /// </summary>
+        /// <param name="priority">The priority.</param>
+        /// <param name="message">The message.</param>
         public void Log(LogPriority priority, string message)
         {
             this.LogPlatform.Log(this.Name, priority, message);
---
+++ CupCake/LogPlatformExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CupCake.Core.Log;
+using JetBrains.Annotations;
+
+namespace CupCake
+{
+    /// <summary>
+    /// Class LoggerExtensions.
+    /// </summary>
+    public static class LoggerExtensions
+    {
+        /// <summary>
+        /// Logs the specified message.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="args">The object array that contains zero or more items to format.</param>
+        [StringFormatMethod("message")]
+        public static void Log(this Logger logger, string message, params object[] args)
+        {
+            // ReSharper disable once RedundantStringFormatCall
+            logger.Log(String.Format(message, args));
+        }
+
+        /// <summary>
+        /// Logs the specified message.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        /// <param name="priority">The priority.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="args">The object array that contains zero or more items to format.</param>
+        [StringFormatMethod("message")]
+        public static void Log(this Logger logger, LogPriority priority, string message, params object[] args)
+        {
+            // ReSharper disable once RedundantStringFormatCall
+            logger.Log(priority, String.Format(message, args));
+        }
+    }
+}
