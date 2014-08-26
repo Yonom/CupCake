f72760715e11e50c92e3f2e672e4b3b020eb300a
Decagon <decagongithub@gmail.com>
2014-08-26 13:07:40 -0300

    Sample comments

--- CupCake.Messages/Receive/AddReceiveEvent.cs
+++ CupCake.Messages/Receive/AddReceiveEvent.cs
@@ -3,8 +3,15 @@ using PlayerIOClient;
 
 namespace CupCake.Messages.Receive
 {
+    /// <summary>
+    /// Class Add Receive Event.
+    /// </summary>
     public class AddReceiveEvent : ReceiveEvent, IUserPosReceiveEvent
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddReceiveEvent"/> class.
+        /// </summary>
+        /// <param name="message">The EE message.</param>
         public AddReceiveEvent(Message message)
             : base(message)
         {
@@ -24,19 +31,75 @@ namespace CupCake.Messages.Receive
             this.IsGuardian = message.GetBoolean(13);
         }
 
+        /// <summary>
+        /// Whether the user is in guardian mode or not.
+        /// </summary>
+        /// <value><c>true</c> if the player has activated guardian mode; otherwise, <c>false</c>.</value>
         public bool IsGuardian { get; set; }
+        /// <summary>
+        /// Returns the amount of coins the player has.
+        /// </summary>
+        /// <value>The coins.</value>
         public int Coins { get; set; }
+        /// <summary>
+        /// Returns the smiley the player has.
+        /// </summary>
+        /// <value>The face.</value>
         public Smiley Face { get; set; }
+        /// <summary>
+        /// Returns whether this player may chat using the free-form chat box.
+        /// </summary>
+        /// <value><c>true</c> if this player has chat; otherwise, <c>false</c>.</value>
         public bool HasChat { get; set; }
+        /// <summary>
+        /// Returns whether this player is a club member.
+        /// </summary>
+        /// <value><c>true</c> if this player is a club member; otherwise, <c>false</c>.</value>
         public bool IsClubMember { get; set; }
+        /// <summary>
+        /// Returns whether this player has activated god mode.
+        /// </summary>
+        /// <value><c>true</c> if this player is in god mode; otherwise, <c>false</c>.</value>
         public bool IsGod { get; set; }
+        /// <summary>
+        /// Returns whether this player is a moderator.
+        /// </summary>
+        /// <value><c>true</c> if this player is a moderator; otherwise, <c>false</c>.</value>
         public bool IsMod { get; set; }
+        /// <summary>
+        /// Returns whether this player is my friend or not.
+        /// </summary>
+        /// <value><c>true</c> if this player is my friend; otherwise, <c>false</c>.</value>
         public bool IsMyFriend { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is purple.?
+        /// </summary>
+        /// <value><c>true</c> if this instance is purple; otherwise, <c>false</c>.</value>
         public bool IsPurple { get; set; }
+        /// <summary>
+        /// Returns the magic class of this player.
+        /// </summary>
+        /// <value>The magic class.</value>
         public MagicClass MagicClass { get; set; }
+        /// <summary>
+        /// Returns the username of the player.
+        /// </summary>
+        /// <value>The username.</value>
         public string Username { get; set; }
+        /// <summary>
+        /// Returns the x coordinate of the player.
+        /// </summary>
+        /// <value>The user position x.</value>
         public int UserPosX { get; set; }
+        /// <summary>
+        /// Returns the y coordinate of the player.
+        /// </summary>
+        /// <value>The user position y.</value>
         public int UserPosY { get; set; }
+        /// <summary>
+        /// Returns the user identifier.
+        /// </summary>
+        /// <value>The user identifier.</value>
         public int UserId { get; set; }
     }
 }
