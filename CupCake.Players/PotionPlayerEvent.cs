32dd619b434b6888ee4f02a40e76858d31063629
Yonom <sepehr1376@gmail.com>
2014-08-28 13:04:14 +0200

    Moar docs

--- CupCake.Players/PotionPlayerEvent.cs
+++ CupCake.Players/PotionPlayerEvent.cs
@@ -2,6 +2,12 @@
 
 namespace CupCake.Players
 {
+    /// <summary>
+    /// Occurs when a potion has been consumed by a player.
+    /// </summary>
+    /// <remarks>
+    /// This event is raised after a <see cref="PotionReceiveEvent"/> has been raised.
+    /// </remarks>
     public class PotionPlayerEvent : PlayerEvent<PotionReceiveEvent>
     {
         internal PotionPlayerEvent(Player oldPlayer, Player player, PotionReceiveEvent innerEvent)
--- CupCake.Players/SayPlayerEvent.cs
+++ CupCake.Players/SayPlayerEvent.cs
@@ -2,6 +2,12 @@
 
 namespace CupCake.Players
 {
+    /// <summary>
+    /// Occurs when a player chats in the world.
+    /// </summary>
+    /// <remarks>
+    /// This event is raised after a <see cref="SayReceiveEvent"/> has been raised.
+    /// </remarks>
     public class SayPlayerEvent : PlayerEvent<SayReceiveEvent>
     {
         internal SayPlayerEvent(Player oldPlayer, Player player, SayReceiveEvent innerEvent)
