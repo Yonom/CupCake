3b059dee2b01732a7050b5e97fd8b4cef982af0a
Yonom <sepehr1376@gmail.com>
2014-08-31 16:30:04 +0200

    Fix SoundBlock names

--- CupCake.Messages/Blocks/SoundBlock.cs
+++ CupCake.Messages/Blocks/SoundBlock.cs
@@ -1,8 +1,15 @@
+using System;
+
 namespace CupCake.Messages.Blocks
 {
     public enum SoundBlock
     {
+        [Obsolete("Use SoundBlock.MusicPiano instead.")]
         BlockMusicPiano = Block.MusicPiano,
-        BlockMusicDrum = Block.MusicDrum
+        [Obsolete("Use SoundBlock.MusicDrum instead.")]
+        BlockMusicDrum = Block.MusicDrum,
+
+        MusicPiano = Block.MusicPiano,
+        MusicDrum = Block.MusicDrum
     }
 }
