2f361389cb151aa3083fb29be38e4cb23b8fbf3e
Yonom <sepehr1376@gmail.com>
2014-09-04 08:45:29 +0200

    Use props file for the post build event, fixes #44

--- CupCake/tools/install.ps1
+++ CupCake/tools/install.ps1
@@ -1,7 +1,5 @@
 param($installPath, $toolsPath, $package, $project)
 
-$project.Properties.Item("PostBuildEvent").Value = "if `$(ConfigurationName) == Debug ""`$(ProjectDir)CupCake.Debug.exe"" Deploy `$(TargetDir)"
-
 $asms = $package.AssemblyReferences | %{$_.Name}
 
 foreach ($reference in $project.Object.References)
